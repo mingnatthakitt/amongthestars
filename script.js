3f3072336a6d16dda60092790f0e7d7b332560e1
mingnatthakitt <159937632+mingnatthakitt@users.noreply.github.com>
2025-03-20 23:26:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
 }
 
 function drawText() {
-    var fontSize = Math.min(20, window.innerWidth / 30); // Adjust font size based on screen width
+    var fontSize = Math.min(24, window.innerWidth / 30); // Adjust font size based on screen width
     var lineHeight = 8;
 
     context.font = fontSize + "px Comic Sans MS";
