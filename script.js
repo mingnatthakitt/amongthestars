369d3df34f2c050e79c0498b06b674ae9f999310
mingnatthakitt <159937632+mingnatthakitt@users.noreply.github.com>
2025-03-20 23:38:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("starfield");
-canvas.width = window.InnerWidth;
-canvas.height = window.InnerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 var context = canvas.getContext("2d");
 var stars = 500;
@@ -55,7 +55,7 @@ function drawTextWithLineBreaks(lines, x, y, fontSize, lineHeight) {
 }
 
 function drawText() {
-    var fontSize = Math.min(22, window.innerWidth / 30); // Adjust font size based on screen width
+    var fontSize = Math.min(22, window.innerWidth / 24); // Adjust font size based on screen width
     var lineHeight = 8;
 
     context.font = fontSize + "px Comic Sans MS";
